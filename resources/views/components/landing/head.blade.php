3dbc8aa6a199f2dab235c62059b635b4125182d9
anharsaja <anharmuk12@gmail.com>
2024-06-17 17:06:52 +0700

    feat: add LandingComponents

---
+++ resources/views/components/landing/head.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+
+	<!-- Meta
+	============================================= -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, intial-scale=1, max-scale=1">
+
+    <meta name="author" content="ExplicitConcepts">
+    <!-- description -->
+    <meta name="description" content="We are ExplicitConcepts Agency. We have experience in web development allows us to create truly Templates in many categories like PSD Templates, HTML Templates, WordPress Themes, Unbounce Themes and Instapage Theme.">
+    <!-- keywords -->
+    <meta name="keywords" content="unbounce, unbounce landing page, instapage, instapage landing page, landing page, web design, PSD template, responsive web design, website designed, free web design, flat web design, site template, web de, website, web development">
+
+	<!-- Stylesheets
+	============================================= -->
+	<link href="{{asset('css/css-assets.css')}}" rel="stylesheet">
+	<link href="{{asset('css/style.css')}}" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Poppins:400i,400,700i,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,600i,700" rel="stylesheet">
+
+	<!-- Favicon
+	============================================= -->
+	<link rel="shortcut icon" href="{{asset('images/general-elements/favicon/favicon.png')}}">
+	<link rel="apple-touch-icon" href="{{asset('images/general-elements/favicon/apple-touch-icon.png')}}">
+	<link rel="apple-touch-icon" sizes="72x72" href="{{asset('images/general-elements/favicon/apple-touch-icon-72x72.png')}}">
+	<link rel="apple-touch-icon" sizes="114x114" href="{{asset('images/general-elements/favicon/apple-touch-icon-114x114.png')}}">
+
+	<!-- Title
+	============================================= -->
+	<title>LeadMark</title>
+
+</head>
+
+<body>
---
+++ resources/views/components/landing/loading.blade.php
@@ -0,0 +1,18 @@
+	<!-- Loading Progress
+	============================================= -->	
+    <div id="loading-progress">
+		<a class="logo" href="#">
+			<img src="images/files/logo-header.png" data-logo-alt="images/files/logo-header-alt.png" alt="">
+			<h3><span class="colored">Smiley</span></h3>
+			<span>HTML Template</span>
+		</a><!-- .logo end -->
+      	<div class="lp-content">
+	        <div class="lp-counter">
+	        	Loading
+	        	<div id="lp-counter">0%</div>
+	        </div><!-- .lp-counter end -->
+	        <div class="lp-bar">
+		        <div id="lp-bar"></div>
+	        </div><!-- .lp-bar end -->
+      	</div><!-- .lp-content end -->
+    </div><!-- #loading-progress end -->

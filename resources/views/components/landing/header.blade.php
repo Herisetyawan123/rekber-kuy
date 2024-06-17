f2e975041afd2de3e1dfec267ee8dc88c0760829
anharsaja <anharmuk12@gmail.com>
2024-06-17 17:06:52 +0700

    feat: add LandingComponents

---
+++ resources/views/components/landing/header.blade.php
@@ -0,0 +1,31 @@
+		<!-- Header
+		============================================= -->
+		<header id="header">
+
+			<div id="header-bar-1" class="header-bar">
+
+				<div class="header-bar-wrap">
+
+					<div class="container">
+						<div class="row">
+							<div class="col-md-12">
+
+								<div class="hb-content">
+									<a class="logo logo-header" href="#">
+										<img src="images/files/logo-header.png" data-logo-alt="images/files/logo-header-alt.png" alt="">
+										<h3><span class="colored">LeadMark</span></h3>
+										<span>HTML Template</span>
+									</a><!-- .logo end -->
+									<div class="hb-meta">
+									</div><!-- .hb-meta end -->
+								</div><!-- .hb-content end -->
+
+							</div><!-- .col-md-12 end -->
+						</div><!-- .row end -->
+					</div><!-- .container end -->
+
+				</div><!-- .header-bar-wrap -->
+
+			</div><!-- #header-bar-1 end -->
+
+		</header><!-- #header end -->

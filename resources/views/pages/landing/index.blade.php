931935888cd8d5704d93a2c108e07b28c61dc0f5
anharsaja <anharmuk12@gmail.com>
2024-06-17 17:06:52 +0700

    feat: add LandingComponents

--- resources/views/pages/landing/index.blade.php
+++ resources/views/pages/landing/index.blade.php
@@ -1,98 +1,14 @@
-
-<!DOCTYPE html>
-<html lang="en-US">
-
-<head>
-
-	<!-- Meta
-	============================================= -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, intial-scale=1, max-scale=1">
-
-    <meta name="author" content="ExplicitConcepts">
-    <!-- description -->
-    <meta name="description" content="We are ExplicitConcepts Agency. We have experience in web development allows us to create truly Templates in many categories like PSD Templates, HTML Templates, WordPress Themes, Unbounce Themes and Instapage Theme.">
-    <!-- keywords -->
-    <meta name="keywords" content="unbounce, unbounce landing page, instapage, instapage landing page, landing page, web design, PSD template, responsive web design, website designed, free web design, flat web design, site template, web de, website, web development">
-
-	<!-- Stylesheets
-	============================================= -->
-	<link href="{{asset('css/css-assets.css')}}" rel="stylesheet">
-	<link href="{{asset('css/style.css')}}" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Poppins:400i,400,700i,700" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,600i,700" rel="stylesheet">
-
-	<!-- Favicon
-	============================================= -->
-	<link rel="shortcut icon" href="{{asset('images/general-elements/favicon/favicon.png')}}">
-	<link rel="apple-touch-icon" href="{{asset('images/general-elements/favicon/apple-touch-icon.png')}}">
-	<link rel="apple-touch-icon" sizes="72x72" href="{{asset('images/general-elements/favicon/apple-touch-icon-72x72.png')}}">
-	<link rel="apple-touch-icon" sizes="114x114" href="{{asset('images/general-elements/favicon/apple-touch-icon-114x114.png')}}">
-
-	<!-- Title
-	============================================= -->
-	<title>LeadMark</title>
-
-</head>
-
-<body>
+<x-landing.head/>
 
 	<div id="scroll-progress"><div class="scroll-progress"><span class="scroll-percent"></span></div></div>
 
-	<!-- Loading Progress
-	============================================= -->	
-    <div id="loading-progress">
-		<a class="logo" href="#">
-			<img src="images/files/logo-header.png" data-logo-alt="images/files/logo-header-alt.png" alt="">
-			<h3><span class="colored">Smiley</span></h3>
-			<span>HTML Template</span>
-		</a><!-- .logo end -->
-      	<div class="lp-content">
-	        <div class="lp-counter">
-	        	Loading
-	        	<div id="lp-counter">0%</div>
-	        </div><!-- .lp-counter end -->
-	        <div class="lp-bar">
-		        <div id="lp-bar"></div>
-	        </div><!-- .lp-bar end -->
-      	</div><!-- .lp-content end -->
-    </div><!-- #loading-progress end -->
-
+    <x-landing.loading/>
+    
 	<!-- Document Full Container
 	============================================= -->
 	<div id="full-container">
 
-		<!-- Header
-		============================================= -->
-		<header id="header">
-
-			<div id="header-bar-1" class="header-bar">
-
-				<div class="header-bar-wrap">
-
-					<div class="container">
-						<div class="row">
-							<div class="col-md-12">
-
-								<div class="hb-content">
-									<a class="logo logo-header" href="#">
-										<img src="images/files/logo-header.png" data-logo-alt="images/files/logo-header-alt.png" alt="">
-										<h3><span class="colored">LeadMark</span></h3>
-										<span>HTML Template</span>
-									</a><!-- .logo end -->
-									<div class="hb-meta">
-									</div><!-- .hb-meta end -->
-								</div><!-- .hb-content end -->
-
-							</div><!-- .col-md-12 end -->
-						</div><!-- .row end -->
-					</div><!-- .container end -->
-
-				</div><!-- .header-bar-wrap -->
-
-			</div><!-- #header-bar-1 end -->
-
-		</header><!-- #header end -->
+        <x-landing.header/>
 
 		<!-- Banner
 		============================================= -->
@@ -116,6 +32,7 @@
 									<div class="description">
 										This should be used to tell a story and let your users know a little more about your product or service. How can you benefit them?
 									</div>
+									<a class="btn popup-btn x-large colorful hover-colorful-darken mt-40" href="#header">> Get Started</a>
 									<div class="cta-subscribe cta-subscribe-1 box-form">
 										<div class="box-content">
 											<div class="cs-notifications">

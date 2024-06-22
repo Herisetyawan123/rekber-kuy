338b88a7db666f535640753c6d132901c95adfee
anharsaja <anharmuk12@gmail.com>
2024-06-23 05:10:55 +0700

    feat: login/register UI

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+
+
+Route::get('/register', function () {
+    return view('pages.auth.register');
+});
+Route::get('/login', function () {
+    return view('pages.auth.login');
+});
 Route::get('/dashboard', function () {
     return view('pages.dashboard.index');
 });

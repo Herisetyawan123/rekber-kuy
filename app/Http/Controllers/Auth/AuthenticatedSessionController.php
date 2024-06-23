9c3dbe3259f3b87dd29636983581e65a06d4975a
Heri Setyawan <herisetyawan233@gmail.com>
2024-06-24 03:11:26 +0700

    feat: logic register and refactor folder logic login

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -1,13 +1,14 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Auth;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class AuthAuthenticatedSessionController extends Controller
+class AuthenticatedSessionController extends Controller
 {
-    public function create()
+   public function create()
     {
         return view('pages.auth.login');
     }

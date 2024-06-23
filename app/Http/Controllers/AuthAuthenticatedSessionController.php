dedbab11435c310bb93d732b7c03cf80454b80e2
Heri Setyawan <herisetyawan233@gmail.com>
2024-06-24 03:08:56 +0700

    feat: logic auth login and logout

---
+++ app/Http/Controllers/AuthAuthenticatedSessionController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthAuthenticatedSessionController extends Controller
+{
+    public function create()
+    {
+        return view('pages.auth.login');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('/');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ]);
+    }
+
+    public function destroy(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}

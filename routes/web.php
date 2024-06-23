d0df05f030ca8d6d26f1aea205bb1fdefcb25aec
anharsaja <anharmuk12@gmail.com>
2024-06-24 02:26:45 +0700

    wip: dashboard

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 
 
 
+Route::get('/chat', function () {
+    return view('pages.chat.index');
+});
 Route::get('/register', function () {
     return view('pages.auth.register');
 });

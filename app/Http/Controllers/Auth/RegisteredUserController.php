081128b3110289a76114670f6c5911c274330639
Heri Setyawan <herisetyawan233@gmail.com>
2024-06-24 04:28:10 +0700

    feat: register auth

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,9 +3,45 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\Auth;
+use App\Providers\RouteServiceProvider;
+use Spatie\Permission\Models\Role;
 
 class RegisteredUserController extends Controller
 {
-    //
+    public function create()
+    {
+        return view('pages.auth.register');
+    }
+
+     public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => ['required'],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $role = Role::where('name', 'user')->first();
+
+        if ($role) {
+            $user->assignRole($role);
+        }
+
+        event(new Registered($user));
+
+        Auth::login($user);
+
+        return redirect()->route('home');
+    }
 }

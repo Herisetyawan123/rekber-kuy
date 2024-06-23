89c8c0056706452ea54a0ef4b9ca33ca5e0698d4
Heri Setyawan <herisetyawan233@gmail.com>
2024-06-24 03:00:28 +0700

    feat: seeder user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+ // Create admin role
+        $adminRole = Role::create(['name' => 'admin']);
+
+        // Create admin user
+        $adminUser = User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@mail.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        // Assign admin role to admin user
+        $adminUser->assignRole($adminRole);
+
+
+        $userRole = Role::create(['name' => 'user']);
+
+        // Create regular user
+        $user = User::create([
+            'name' => 'Regular User',
+            'email' => 'user@mail.com',
+            'password' => Hash::make('password'),
+        ]);
+        
+        // Assign admin role to admin user
+        $user->assignRole($userRole);
+    }
+}

b22409300399e4529cd9ca7c3fd5a68fcf3e6957
Heri Setyawan <herisetyawan233@gmail.com>
2024-06-26 13:38:41 +0700

    feat: erd room chat and transaction

---
+++ app/Models/Withdrawal.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Withdrawal extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+}

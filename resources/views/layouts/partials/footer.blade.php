dc716f8131b3e02e9aed0ff457819207b020c788
anharsaja <anharmuk12@gmail.com>
2024-06-26 19:23:15 +0700

    feat: UI user detail

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -2,29 +2,40 @@
 <div class="rightbar-overlay"></div>
 
 <!-- JAVASCRIPT -->
-<script src="{{asset('assets/libs/jquery/jquery.min.js')}}"></script>
-<script src="{{asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-<script src="{{asset('assets/libs/metismenu/metisMenu.min.js')}}"></script>
-<script src="{{asset('assets/libs/simplebar/simplebar.min.js')}}"></script>
-<script src="{{asset('assets/libs/node-waves/waves.min.js')}}"></script>
-<script src="{{asset('assets/libs/feather-icons/feather.min.js')}}"></script>
+<script src="{{ asset('assets/libs/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('assets/libs/metismenu/metisMenu.min.js') }}"></script>
+<script src="{{ asset('assets/libs/simplebar/simplebar.min.js') }}"></script>
+<script src="{{ asset('assets/libs/node-waves/waves.min.js') }}"></script>
+<script src="{{ asset('assets/libs/feather-icons/feather.min.js') }}"></script>
 <!-- pace js -->
-<script src="{{asset('assets/libs/pace-js/pace.min.js')}}"></script>
+<script src="{{ asset('assets/libs/pace-js/pace.min.js') }}"></script>
 
 <!-- apexcharts -->
-<script src="{{asset('assets/libs/apexcharts/apexcharts.min.js')}}"></script>
+<script src="{{ asset('assets/libs/apexcharts/apexcharts.min.js') }}"></script>
 
 <!-- Plugins js-->
-<script src="{{asset('assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js')}}"></script>
-<script src="{{asset('assets/libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-world-mill-en.js')}}"></script>
+<script src="{{ asset('assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js') }}"></script>
+<script src="{{ asset('assets/libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-world-mill-en.js') }}">
+</script>
 <!-- dashboard init -->
+<script src="{{ asset('assets/js/pages/dashboard.init.js') }}"></script>
 
+<script src="{{ asset('assets/js/app.js') }}"></script>
+
+
+
+<!-- JAVASCRIPT -->
+<script src="{{asset('assets/libs/jquery/jquery.min.js')}}"></script>
+<script src="{{asset('assets/libs/pace-js/pace.min.js')}}"></script>
+
+<!-- twitter-bootstrap-wizard js -->
 <script src="{{asset('assets/libs/twitter-bootstrap-wizard/jquery.bootstrap.wizard.min.js')}}"></script>
 <script src="{{asset('assets/libs/twitter-bootstrap-wizard/prettify.js')}}"></script>
 
-<script src="{{asset('assets/js/pages/dashboard.init.js')}}"></script>
+<!-- form wizard init -->
+<script src="{{asset('assets/js/pages/form-wizard.init.js')}}"></script>
 
-<script src="{{asset('assets/js/app.js')}}"></script>
 
 </body>
 

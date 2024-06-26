8ebe0b14996cfb5a58c8b97954db14509386454c
Heri Setyawan <herisetyawan233@gmail.com>
2024-06-26 13:38:41 +0700

    feat: erd room chat and transaction

---
+++ database/migrations/2024_06_26_062208_create_payments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('transaction_id')->constrained('transactions')->onDelete('cascade');
+            $table->string('payment_method');
+            $table->decimal('amount', 15, 2);
+            $table->enum('status', ['unverified', 'verified']);
+            $table->foreignId('admin_id')->nullable()->constrained('users')->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};

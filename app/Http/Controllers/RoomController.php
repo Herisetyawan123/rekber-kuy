f4b33264a2bb60d23cab5d7c50d247f314011976
anharsaja <anharmuk12@gmail.com>
2024-07-01 00:12:36 +0700

    feat: create room seller chat

---
+++ app/Http/Controllers/RoomController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chat;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class RoomController extends Controller
+{
+    public function generateCode() {
+        $characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $code = 'NG-';
+        for ($i = 0; $i < 4; $i++) {
+            $code .= $characters[random_int(0, strlen($characters) - 1)];
+        }
+        return $code;
+    }
+
+    public function index()
+    {
+        $rooms = Chat::where('user_id', '=', Auth::user()->id)->get();
+        return view('pages.chat.index', compact('rooms'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        Chat::create([
+            'code' => $this->generateCode(),
+            'title' => $request->title,
+            'user_id' => Auth::user()->id,
+        ]);
+
+        return redirect()->back()->with('success', 'room berhasil di buat');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return view('pages.chat.detail');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}

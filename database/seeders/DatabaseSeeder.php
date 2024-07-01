45f1c5a7ab65b42ee3530980e4897f8d639b2a2e
anharsaja <anharmuk12@gmail.com>
2024-07-02 01:34:15 +0700

    feat: add seeder room, user, rekening, profile

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,11 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-          $this->call([
+        $this->call([
             UserSeeder::class,
             RoomSeeder::class,
+            ProfileSeeder::class,
+            RekeningSeeder::class,
         ]);
 
     }

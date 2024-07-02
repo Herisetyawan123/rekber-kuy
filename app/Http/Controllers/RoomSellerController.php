75b7ba8ba0560d6625cc7ebd1e2994b0394e3e27
anharsaja <anharmuk12@gmail.com>
2024-07-03 02:44:19 +0700

    feat: room orderby-desc

--- app/Http/Controllers/RoomSellerController.php
+++ app/Http/Controllers/RoomSellerController.php
@@ -27,7 +27,7 @@ class RoomSellerController extends Controller
 
     public function index()
     {
-        $rooms = Chat::where('seller_id', '=', Auth::user()->id)->get();
+        $rooms = Chat::where('seller_id', '=', Auth::user()->id)->orderBy('id',  'DESC')->get();
         return view('pages.room.index', compact('rooms'));
     }
 
@@ -50,8 +50,7 @@ class RoomSellerController extends Controller
             'seller_id' => Auth::user()->id,
         ]);
 
-        return redirect()->back()->with('success', 'room berhasil di buat');
-        // return response()->json(['code' => 'room berhasil di buat']);
+        return redirect()->back()->with('success', '');
     }
 
     /**

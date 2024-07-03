47bc8e716294e01ab2aeaa5ef008a8b74e57a8dd
anharsaja <anharmuk12@gmail.com>
2024-07-04 05:00:48 +0700

    feat: fixup UI navbar

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -6,45 +6,49 @@
                         <ul class="navbar-nav">
 
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle arrow-none" href="{{ route('home') }}" id="topnav-dashboard"
-                                    role="button">
+                                <a class="nav-link dropdown-toggle arrow-none" href="{{ route('home') }}"
+                                    id="topnav-dashboard" role="button">
                                     <i data-feather="home"></i><span data-key="t-dashboards">Dashboard</span>
                                 </a>
                             </li>
 
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle arrow-none" href="javascript:void(0);" id="topnav-pages"
-                                    role="button">
-                                    <i data-feather="arrow-up"></i><span data-key="t-components">Penjual</span>
+                                <a class="nav-link dropdown-toggle arrow-none" href="javascript:void(0);"
+                                    id="topnav-pages" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <i data-feather="file-plus"></i>
+                                    <span data-key="t-apps">Penjual</span>
                                     <div class="arrow-down"></div>
                                 </a>
                                 <div class="dropdown-menu" aria-labelledby="topnav-pages">
 
-                                    <a href="{{ route('room-seller.index') }}" class="dropdown-item" data-key="t-forms">Room Chat</a>
-
+                                    <a href="{{ route('room-seller.index') }}" class="dropdown-item"
+                                        data-key="t-calendar">Room Chat Penjual</a>
                                     <a href="{{ route('transaksi.penjual') }}" class="dropdown-item"
-                                    data-key="t-tables">Transaksi List</a>
+                                        data-key="t-chat">Transaksi Penjual</a>
+
                                 </div>
                             </li>
 
-                            
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle arrow-none" href="javascript:void(0);" id="topnav-pages"
-                                    role="button">
-                                    <i data-feather="arrow-down"></i><span data-key="t-extra-pages">Pembeli</span>
+                                <a class="nav-link dropdown-toggle arrow-none" href="javascript:void(0);"
+                                    id="topnav-pages" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <i data-feather="shopping-cart"></i>
+                                    <span data-key="t-apps">Pembeli</span>
                                     <div class="arrow-down"></div>
                                 </a>
                                 <div class="dropdown-menu" aria-labelledby="topnav-pages">
 
-                                    <a href="{{ route('room-buyer.index') }}" class="dropdown-item" data-key="t-authentication">Room Chat</a>
-
+                                    <a href="{{ route('room-buyer.index') }}" class="dropdown-item"
+                                        data-key="t-calendar">Room Chat Pembeli</a>
                                     <a href="{{ route('transaksi.pembeli') }}" class="dropdown-item"
-                                    data-key="t-utility">Transaksi List</a>
+                                        data-key="t-chat">Transaksi Pembeli</a>
+
                                 </div>
                             </li>
+
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle arrow-none" href="{{ route('invoice') }}" id="topnav-dashboard"
-                                    role="button">
+                                <a class="nav-link dropdown-toggle arrow-none" href="{{ route('invoice') }}"
+                                    id="topnav-dashboard" role="button">
                                     <i data-feather="book"></i><span data-key="t-invoice">Invoice</span>
                                 </a>
                             </li>
